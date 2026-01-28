398b7a5ef97915b8e2199490cbd4d3682cefdec1
Dulara Wickramanayaka <dularaw02@gmail.com>
2026-01-28 14:01:24 +0000

    Add error handler for internal server errors

--- src/App.js
+++ src/App.js
@@ -194,3 +194,13 @@ app.use((req, res) => {
     message: 'Route not found'
   });
 });
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: 'Internal server error',
+    error: process.env.NODE_ENV === 'development' ? err.message : undefined
+  });
+});
